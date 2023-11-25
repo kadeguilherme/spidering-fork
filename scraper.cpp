b8f5b67ff2e2b0c4804c423f56cadba4bb710716
kadeguilherme <kadeguilherme@gmail.com>
2023-11-25 16:08:05 -0300

    Pegando os pdfs da pagina

--- scraper.cpp
+++ scraper.cpp
@@ -45,24 +45,28 @@ std::string GetHtmlFromUrl(const std::string& url) {
 int main() {
     std::string url = "https://www.ifb.edu.br/";
     std::string html = GetHtmlFromUrl(url);
-    std::regex hrefRegex("href=\"([^\"]*ifb[^\"]*)\"");
-    std::regex pdfRegex("[^.]*.pdf");
+    std::regex hrefRegex("href=\"([^\"]*ifb[^\"]*)\"|[^.]*.pdf");
+    //std::regex pdfRegex("[^.]*.pdf");
     std::map<std::string,std::string> my_link;
 
     auto it = std::sregex_iterator(html.begin(), html.end(), hrefRegex);
     auto end = std::sregex_iterator();
 
-    auto links = std::sregex_iterator(html.begin(), html.end(), pdfRegex);
+   // auto links = std::sregex_iterator(html.begin(), html.end(), pdfRegex);
 
     // Percorrer as correspondências e imprimir os valores de href
     for (; it != end; ++it) {
         std::smatch match = *it;
         //std::cout << "Href encontrado: " << match[1].str() << std::endl;
-        my_link.insert(std::make_pair( match[1].str() ,match[1].str()));
+        for (int i = 1; i < match.size(); i++) {
+            if (!match[i].str().empty()) {
+                my_links.insert(std::make_pair(match[i].str(), match[i].str()));
+            }
+        }
     }
 
     std::cout << my_link;
-    std::cout << links;
+    //std::cout << links;
     
 
     return 0;
