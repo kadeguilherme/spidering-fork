13c2c2c22a224ac2c30cab31301a054186120460
kadeguilherme <kadeguilherme@gmail.com>
2023-11-25 16:11:54 -0300

    Pegando os pdfs da pagina

--- scraper.cpp
+++ scraper.cpp
@@ -24,6 +24,7 @@ std::string GetHtmlFromUrl(const std::string& url) {
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
 
+        // Desabilitar a verificação do certificado SSL (use isso com cuidado)
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 
         res = curl_easy_perform(curl);
@@ -45,29 +46,29 @@ std::string GetHtmlFromUrl(const std::string& url) {
 int main() {
     std::string url = "https://www.ifb.edu.br/";
     std::string html = GetHtmlFromUrl(url);
+
+    // Regex para encontrar URLs com "ifb" e links que terminam com ".pdf"
     std::regex hrefRegex("href=\"([^\"]*ifb[^\"]*)\"|[^.]*.pdf");
-    //std::regex pdfRegex("[^.]*.pdf");
-    std::map<std::string,std::string> my_link;
+
+    std::map<std::string, std::string> my_links;
 
     auto it = std::sregex_iterator(html.begin(), html.end(), hrefRegex);
     auto end = std::sregex_iterator();
 
-   // auto links = std::sregex_iterator(html.begin(), html.end(), pdfRegex);
-
-    // Percorrer as correspondências e imprimir os valores de href
+    // Percorrer as correspondências e adicionar ao mapa
     for (; it != end; ++it) {
         std::smatch match = *it;
-        //std::cout << "Href encontrado: " << match[1].str() << std::endl;
         for (int i = 1; i < match.size(); i++) {
             if (!match[i].str().empty()) {
-                my_link.insert(std::make_pair(match[i].str(), match[i].str()));
+                my_links.insert(std::make_pair(match[i].str(), match[i].str()));
             }
         }
     }
 
-    std::cout << my_link;
-    //std::cout << links;
-    
+    // Imprimir os links
+    for (const auto& link : my_links) {
+        std::cout << "Link encontrado: " << link.first << std::endl;
+    }
 
     return 0;
 }
